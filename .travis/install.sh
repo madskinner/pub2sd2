cc6fbb149c9a58117fa923ae2a39d090fe9ab830
madskinner <mark_skinner@sil.org>
2017-12-06 08:38:31 +0000

    matrix-2

---
+++ .travis/install.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+
+    # Install some custom requirements on OS X
+    # e.g. brew install pyenv-virtualenv
+
+    case "${TOXENV}" in
+        py34)
+            # Install some custom Python 3.4 requirements on OS X
+            ;;
+        py35)
+            # Install some custom Python 3.5 requirements on OS X
+            ;;
+        py36)
+            # Install some custom Python 3.6 requirements on OS X
+            ;;
+    esac
+else
+    # Install some custom requirements on Linux
+fi
