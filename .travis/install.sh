823f74e7407996cda450af319439808d5a16e07e
madskinner <mark_skinner@sil.org>
2017-12-06 08:59:02 +0000

    matrix-5

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,22 +8,22 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     case "${TOXENV}" in
         py34)
             # Install some custom Python 3.4 requirements on OS X
-           "pip install ."
-           "pip install -r requirements.txt"
+           pip install .
+           pip install -r requirements.txt
             ;;
         py35)
             # Install some custom Python 3.5 requirements on OS X
-           "pip install ."
-           "pip install -r requirements.txt"
+           pip install .
+           pip install -r requirements.txt
             ;;
         py36)
             # Install some custom Python 3.6 requirements on OS X
-            "pip install ."
-            "pip install -r requirements.txt"
+            pip install .
+            pip install -r requirements.txt
             ;;
     esac
 else
     # Install some custom requirements on Linux
-    "pip install ."
-    "pip install -r requirements.txt"
+    pip install .
+    pip install -r requirements.txt
 fi
