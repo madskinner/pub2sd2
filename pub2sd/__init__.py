68f6740b22764bc4aa6771c0763ccd0039af9285
madskinner <mark_skinner@sil.org>
2017-12-08 10:18:47 +0000

    0.9.9.20b

--- pub2sd/__init__.py
+++ pub2sd/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #from __future__ import print_function
 #from ._version import get_versions
-from myclasses.myconst.therest import THIS_VERSION
+from .myclasses.myconst.therest import THIS_VERSION
 __author__ = 'Mark Skinner'
 __email__ = 'mark_skinner@sil.org'
 #__version__ = get_versions()['version']
